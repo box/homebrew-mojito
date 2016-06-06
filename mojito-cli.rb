1c380fc91421cf29ba5c702bf2bf7506d0269c79
Jean Aurambault <jaurambault@box.com>
2016-06-06 13:22:45 -0700

    fix version

--- mojito-cli.rb
+++ mojito-cli.rb
@@ -5,10 +5,10 @@ class MojitoCli < Formula
   #url "https://github.com/box/mojito/releases/download/v0.33/mojito-cli-0.33.jar"
   #sha256 "53c0af93fbd512f72fd56d7307cc0d0ab1d23e70ed170496ea7b80ea074c2ea2"
 
-  url "git@github.com:box/mojito.git", :using => :git, :tag => "v0.31"
-  version "0.31"
+  url "git@github.com:box/mojito.git", :using => :git, :tag => "v0.33"
+  version "0.33"
 
-  head "git@github.com:box/mojito.git", :using => :git, :branch => "master" # , :tag => "v0.31"
+  head "git@github.com:box/mojito.git", :using => :git, :branch => "master" # , :tag => "v0.33"
 
   #depends_on :java => "1.7+"
   depends_on "maven" => :build
