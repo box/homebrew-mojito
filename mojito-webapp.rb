19d4a25261bacdce927f920b11f7a912aa2b4646
Jean Aurambault <aurambaj@users.noreply.github.com>
2016-08-17 11:55:43 -0700

    first commit

---
+++ mojito-webapp.rb
@@ -0,0 +1,34 @@
+class MojitoWebapp < Formula
+  desc "Mojito Webapp, is the server of Mojito: a continuous localization platform"
+  homepage "http://mojito.global"
+  
+  url "https://github.com/box/mojito/releases/download/v0.52/mojito-webapp-0.52.jar"
+  sha256 "53c0af93fbd512f72fd56d7307cc0d0ab1d23e70ed170496ea7b80ea074c2ea2"
+
+  head "git@github.com:box/mojito.git", :using => :git, :branch => "master"
+
+  depends_on :java => "1.7+"
+
+  if build.head?
+    depends_on "maven" => :build
+  end
+
+  def install
+ 
+    if build.head?
+      # build the jar
+      system "mvn package -DskipTests"
+      libexec.install Dir["webapp/target/mojito-webapp-*.jar"]
+    else
+      # use downloaded jar
+      libexec.install Dir["mojito-webapp-*.jar"]         
+    end
+
+    # Create the shell script to execute mojito webapp
+    (bin/"mojito-webapp").write <<-EOS.undent
+          #!/bin/sh
+          java -XX:MaxPermSize=128m -Xmx1024m -Dspring.config.location=#{etc}/mojito/webapp/ -jar #{libexec}/mojito-webapp-*.jar "$@"
+    EOS
+
+  end
+end
